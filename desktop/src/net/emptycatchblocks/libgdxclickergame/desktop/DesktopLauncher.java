af39a9035362caf42bdde15b23d3503266a818d4
Adam Hilton <adam.tyler.hilton@gmail.com>
2017-09-05 21:01:27 -0500

    Create libgdx android/desktop project with ashley and tools modules

---
+++ desktop/src/net/emptycatchblocks/libgdxclickergame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.emptycatchblocks.libgdxclickergame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.emptycatchblocks.libgdxclickergame.ClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ClickerGame(), config);
+	}
+}
